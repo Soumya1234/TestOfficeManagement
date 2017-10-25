be5452552c5f1317f6fe792d5237160f77a8e64e
Soumya@1234 <soumyadeepganguly41@gmail.com>
2017-10-25 16:52:19 +0530

    Office class now works

--- src/Code/Office.java
+++ src/Code/Office.java
@@ -3,12 +3,14 @@ package Code;
 public class Office {
      private String Office_Code;
      private Office Controlling_Office;
+     private int No_Of_Children;
      private int Level;
      private static int numOfLevels=1;
      
      public Office()
      {
     	 Controlling_Office=null;
+    	 No_Of_Children=0;
     	 Level=1;
     	 setOfficeCode();
      }
@@ -17,9 +19,14 @@ public class Office {
      {
     	 Controlling_Office=controlling_office;
     	 Level=Controlling_Office.getOfficeLevel()+1;
+    	 setCOChildren();
     	 setOfficeCode();
      }
      
+     private void setCOChildren() //increment Controlling Office's noOfChildren once this office is created
+     {
+    	 Controlling_Office.No_Of_Children++;
+     }
      private void setOfficeCode()
      {
     	 if(Level==1)
@@ -34,8 +41,10 @@ public class Office {
     		 }
     	 }
     	 else
-    	 {
-    		 Office_Code=Controlling_Office.getOfficeCode().substring(, arg1)
+    	 {   
+    		 int noOfZeroPadding=2;
+    		 Office_Code=Integer.toString(Integer.parseInt((Controlling_Office.getOfficeCode()).substring(0, (noOfZeroPadding*Level-1)))+Controlling_Office.No_Of_Children)+
+    				 Controlling_Office.getOfficeCode().substring(noOfZeroPadding*Level-1);
     	 }
      }
      
@@ -46,11 +55,11 @@ public class Office {
      
      private String paddzero()
      {
-    	 String result="000";
+    	 String result="00";
     	 
     	 for(int i=0;i<numOfLevels-2;i++)
     	 {
-    		 result=result+"000";
+    		 result=result+"00";
     	 }
     	 return result;
      }
--- src/Code/TestOffice.java
+++ src/Code/TestOffice.java
@@ -4,15 +4,29 @@ public class TestOffice {
 
 	public static void main(String[] args) 
 	{
-		Office.setNumberOfLevels(4);
+		Office.setNumberOfLevels(5);
 		Office a=new Office();
 		Office b=new Office(a);
 		Office c=new Office(b);
 		Office d=new Office(c);
+		Office e=new Office(d);
+		Office f=new Office(a);
+		Office g=new Office(b);
 		System.out.println(a.getOfficeCode());
+		System.out.println(a.getOfficeLevel());
 		System.out.println(b.getOfficeCode());
+		System.out.println(b.getOfficeLevel());
 		System.out.println(c.getOfficeCode());
+		System.out.println(c.getOfficeLevel());
 		System.out.println(d.getOfficeCode());
+		System.out.println(d.getOfficeLevel());
+		System.out.println(e.getOfficeCode());
+		System.out.println(e.getOfficeLevel());
+		System.out.println(f.getOfficeCode());
+		System.out.println(f.getOfficeLevel());
+		System.out.println(g.getOfficeCode());
+		System.out.println(g.getOfficeLevel());
+		
 	}
 
 }
